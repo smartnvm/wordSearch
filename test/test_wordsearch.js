9c4ae82121728e1aa786c0fd38f87386c7ce3cbe
AJ <aj@smartnvm.com>
2021-09-22 22:16:24 +0000

    code bug fixes, add more test cases

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,36 +3,45 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
-describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
-    const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
 
+let lettersArr = [
+  ['A', 'W', 'C', 'F', 'R', 'A', 'K', 'K'],
+  ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+  ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+  ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+  ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+  ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+  ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+  ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+]
+
+describe("#wordSearch()", function () {
+  it("should return false if the word is not present", function () {
+    const result = wordSearch(lettersArr, 'FRANKS')
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
-    const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+  it("should return true if the word is present", function () {
+    const result = wordSearch(lettersArr, 'SEINFELD')
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function () {
+    const result = wordSearch(lettersArr, 'LARRY')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the array is empty", function () {
+    const result = wordSearch([], 'FRIEND')
+    assert.isFalse(result);
+  });
+
+  it("should return 'Error' if the array is undefined", function () {
+    const result = wordSearch(undefined, 'FRIEND')
+    assert.strictEqual('Error', result);
+  });
 });
+
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,42 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+const horizontalJoin = (letters, word) => {
+  const horizontal = letters.map(ls => ls.join(''))
+  for (l of horizontal) {
+      if (l.includes(word)) return true
+  }
+}
+
+const transpose = function (matrix) {
+  let rows = matrix.length          
+  let cols = matrix[0].length       
+  let transposeArray = [];   
+  let tempArry = []
+
+  for (let c = 0; c < cols; c++) {
+      tempArry = []  //helper array
+      for (let r = 0; r < rows; r++) {
+          tempArry.push(matrix[r][c])
+      }
+      transposeArray.push(tempArry)
+  }
+
+  return transposeArray
+}
+
+const wordSearch = (letters, word) => {
+  if (letters === undefined) {
+      return 'Error' 
+  }
+  if (letters.length === 0) {
+      console.log('emtpry array')
+      return false
+  }
+  if (horizontalJoin(letters, word)) {
+      return true
+  }
+  if (horizontalJoin(transpose(letters), word)) {
+      return true
+  }
+  return false;
 }
 
 module.exports = wordSearch
